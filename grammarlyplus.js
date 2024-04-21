1ce691a2103a8116da515ad52d4a88b2cb71f128
Anson Li <130667068+28lian@users.noreply.github.com>
2024-04-20 21:39:23 -0500

    Update grammarlyplus.js

--- grammarlyplus.js
+++ grammarlyplus.js
@@ -1,4 +1,4 @@
-/// grammarly_plus.js
+/// grammarly.js
 setInterval(() => {
     try {
             for (let premiums of document.querySelector("grammarly-popups").shadowRoot.querySelectorAll("button.sticker")) {
